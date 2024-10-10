3d09153f58d9f180a67d3105f67c1d31e1a9e35d
julian <juliangiles@hotmail.com.ar>
2024-10-10 17:13:08 -0300

    medio que implementamos el objeto dibujable

--- drawable.cpp
+++ drawable.cpp
@@ -1,3 +1,19 @@
 #include "drawable.h"
 
-Drawable::Drawable() {}
+Drawable::Drawable(P_R, c) {
+    this ->P_Rel = P_R;
+    this ->update_p(c);
+}
+
+Drawable::Draw(){
+    QPainter PoliLinea(P_Abs);
+    PoliLinea.drawPolygon();
+}
+
+Drawable::update_p(){
+
+
+}
+
+
+
--- drawable.h
+++ drawable.h
@@ -1,10 +1,24 @@
 #ifndef DRAWABLE_H
 #define DRAWABLE_H
+#include <QPainter>
+#include <QPointF>
+#include <QList>
 
 class Drawable
 {
+
+protected:
+
+QList<QPointF> P_Rel;
+QPointF centroMasa;
+QList<QPointF> P_Abs;
+
 public:
-    Drawable();
+    Drawable(P_R, c);
+    virtual void Draw();
+    virtual void update_p(P);//p es el nuvo centro
+
+
 };
 
 #endif // DRAWABLE_H
