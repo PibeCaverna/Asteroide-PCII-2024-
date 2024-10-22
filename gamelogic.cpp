d29c9e0f295854d3c7cb51cfe075df28094a9054
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2024-10-22 16:13:29 -0300

    Arranqué a laburar escalado de ventana

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -1,8 +1,7 @@
 #include "gamelogic.h"
 
-GameLogic::GameLogic(int Ancho, int Alto) {
-    this->_ancho=Ancho;
-    this->_alto=Alto;
+GameLogic::GameLogic() {
+
 
     this ->_nave = new PJ(QPointF(800,600));
 
@@ -17,9 +16,7 @@ GameLogic::GameLogic(int Ancho, int Alto) {
 }
 
 void GameLogic::Dibujar(QPainter * p){
-    p->setWindow(0,0,2*_ancho,2*_alto);
-    p->setViewport(0,0,_ancho,_alto);
-    p->fillRect(0,0,_ancho,_alto,Qt::black);
+    p->fillRect(0,0,3200,2400,Qt::black);
     for(Drawable *D : dibujables)
     {
         D->Dibujar(p);
