286bddf18a0035ccc5d0843acda831a1f089c90d
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2024-10-31 14:48:16 -0300

    inicios de ui

--- ui.cpp
+++ ui.cpp
@@ -1,4 +1,21 @@
 #include "ui.h"
 
 
-UI::UI() {}
+UI::UI() {
+    this -> _Puntos = 0;
+    this -> _Vidas = 0;
+}
+
+void UI::Dibujar(QPainter *p){
+    QString Texto = ("Score: ");
+    QPointF dibuvida[5] = _Formavida;
+
+    Texto += QString::number(_Puntos);
+    p ->drawText(_txtanchor,Texto);
+    for (int i = 0; i < _Vidas; i++){
+        for (int j = 0; j < 5;j++){
+            dibuvida[j] += QPointF(25,0);
+        }
+        p ->drawPolygon(Dibuvida,5);
+    }
+}
--- ui.h
+++ ui.h
@@ -1,10 +1,27 @@
 #ifndef UI_H
 #define UI_H
+#include "drawable.h"
 
 class UI
 {
 public:
     UI();
+    UI operator+=(int);
+    void Dibujar(QPainter *p) override;
+    void Update() override;
+    int auch();
+    int oneup();
+protected:
+    int _Puntos;
+    int _vidas;
+    QPointF _txtanchor = QPointF(3200,200);
+    QPointF _Formavida[5] = (
+        QPointF( 25, 10),
+        QPointF( 35, 35),
+        QPointF( 29, 31),
+        QPointF( 21, 31),
+        QPointF( 11, 35),);
+
 };
 
 #endif // UI_H
